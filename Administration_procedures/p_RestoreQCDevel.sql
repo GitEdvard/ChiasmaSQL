d9cc5b117dd3b8820d7c4d67bfb8b835a712dc13
GitEdvard <edvard.englund@medsci.uu.se>
2018-09-06 13:40:32 +0200

    Add backup and restore procs for sqat

---
+++ Administration_procedures/p_RestoreQCDevel.sql
@@ -0,0 +1,61 @@
+use Administration
+GO
+
+/****** Object:  StoredProcedure [dbo].[p_CloneGTDB2DevelLatestRelease]    Script Date: 2014-02-27 14:07:56 ******/
+SET ANSI_NULLS OFF
+GO
+
+SET QUOTED_IDENTIFIER OFF
+GO
+
+
+-- Restore GTDB2_devel from backup. 
+-- Parameters:
+-- @latest_version			Restores from the latest release backup
+-- @latest_merge			Restores from the latest merge backup
+
+create procedure p_RestoreQCDevel @latest_release bit = 1, @latest_merge bit = 0
+
+as
+begin
+SET NOCOUNT ON
+
+declare @devel_db_name varchar(255)
+declare @sql varchar(max)
+declare @backup_file varchar(1024)
+
+set @devel_db_name = 'QC_devel'
+
+if @latest_merge = 1 begin
+	set @backup_file = '''D:\Proc_references\Devel_backups\Latest merge\qc_devel_backup.bak'''
+end
+if @latest_release = 1 begin
+	set @backup_file = '''D:\Proc_references\Devel_backups\Latest release\qc_devel_backup.bak'''
+end
+
+-- Restore backup from latest release folder
+set @sql = 
+'if exists (select name from Administration.dbo.sysdatabases where name = ''' + @devel_db_name + ''') begin
+	alter database ' + @devel_db_name + ' set single_user with rollback immediate
+end
+'
+
+set @sql = @sql + 
+'restore database ' + @devel_db_name + ' from disk = '+ @backup_file + '
+with 
+replace, recovery
+
+alter database ' + @devel_db_name + ' set multi_user
+'
+
+--select @sql
+--return
+
+exec (@sql)
+
+
+SET NOCOUNT off
+end
+go
+
+
